0d512f3c328fccb0585801feb2a632e65156fa28
Louis <lssayers@gmail.com>
2011-10-11 09:15:29 +0100

    Louis can get to an edit page, can't yet save an edited page though...

--- app.rb
+++ app.rb
@@ -3,13 +3,23 @@ require 'haml'
 require './models'
 
 
+get %r{^(?!/admin/.*$)(.*)} do |page_name|
+  page = Page.where(name: page_name).first
+  return haml :error_page if page.nil?
+
+  return haml :html_content, :locals => {:content => page.content} if page.markup == 'html'
+  return haml page.content.to_s if page.markup == 'haml'
+
+  return haml :error_page
+end
+
 get '/admin/' do
   @all_pages = Page.all
   haml :'admin/index', :layout => :'admin/layout'
 end
 
 get '/admin/page/create' do
-  haml :'/admin/create_a_page', :layout => :'admin/layout'
+  haml :'/admin/page', :layout => :'admin/layout'
 end
 
 post '/admin/page/create' do
@@ -17,8 +27,18 @@ post '/admin/page/create' do
   redirect '/admin/'
 end
 
+get '/admin/page/edit/:page_id' do
+  @page = Page.where(_id: params[:page_id]).first
+  haml :'/admin/page', :layout => :'admin/layout'
+end
+
+post '/admin/page/save/:page_id' do
+  #Update the page Page.update(params[:page_id], params)
+  redirect '/admin/'
+end
+
 get '/admin/page/delete/:page_id' do
   Page.where(_id: params[:page_id]).delete
   redirect '/admin/'
-end
+  end
 
